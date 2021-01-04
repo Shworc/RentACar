9433419bd9237b829f8bef7003e8c4a2a44c4083
Sasa Filipovic <229-2015@ftn.edu.rs>
2021-01-04 14:05:53 +0100

    Forma za registraciju novog korisnika

---
+++ RentAcar/PresentationLayer/FormRegistration.cs
@@ -0,0 +1,40 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class FormRegistration : Form
+    {
+        public FormRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            int InsertZakupac(Zakupac z)
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand();
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandText = string.Format("INSERT INTO Teams VALUES ('{0}','{1}')",
+                        z.Ime, z.Password);
+
+                    sqlConnection.Open();
+
+                    return sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}

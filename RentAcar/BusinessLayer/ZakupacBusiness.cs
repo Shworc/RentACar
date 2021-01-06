18ad6d1d7b9565bf6aeb3563c203ce3f98a13fc1
Vladimir Djurkovic <225-2017@ftn.edu.rs>
2021-01-06 09:00:33 +0100

    dodat Zakupac biznis

---
+++ RentAcar/BusinessLayer/ZakupacBusiness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class ZakupacBusiness
+    {
+        private readonly ZakupacRepository zakupacRepository;
+
+        public ZakupacBusiness()
+        {
+            this.zakupacRepository = new ZakupacRepository();
+        }
+        public bool InsertZakupac(Zakupac r)
+        {
+            if (this.zakupacRepository.InsertZakupac(r) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}

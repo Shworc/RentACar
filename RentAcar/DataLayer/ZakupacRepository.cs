811a7fd171cd66d350eebf986659d48dfe32a2f3
Branislav Kondic <101-2015@ftn.edu.rs>
2021-01-06 08:52:10 +0100

    Dodat Zakupac repository, insert fja

---
+++ RentAcar/DataLayer/ZakupacRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ZakupacRepository
+    {
+        int InsertZakupac(Zakupac z)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = string.Format("INSERT INTO Teams VALUES ('{0}','{1}')",
+                    z.Ime, z.Password);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}

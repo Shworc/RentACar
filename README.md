006e8bc7e3b8b262fc3e1bc03f23097cd15ae0c3
Branislav <banekondic1996>
2021-01-23 14:00:39 +0100

    Delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# RentACar
-Projektni zadataka iz Softverskog inzenjerstva
--- dbo.Zakupac.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Zakupac] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [Ime]      NVARCHAR (100) NOT NULL,
-    [Password] NVARCHAR (50)  NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
